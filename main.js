17c3b3260c475032c0ead1c019492e88dc2203c7
Locky <inox127@hotmail.com>
2018-11-12 16:37:55 +0100

    added lines

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-tabCartes = [1,2,3,4,5,6,7,8,9,10];
+tabCartes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-cartesJ1 = document.querySelectorAll(".container-j1").length;
+cartesJ1 = document.querySelector(".container-j1").getElementsByTagName("div");
 
-cartesJ1.innerHTML= `${tabCartes[Math.floor((Math.random() * (tabCartes.length)-1)+1)]}`;
 
-console.log("test");
+for (i = 0; i < 5; i++) {
+    let rand = tabCartes[Math.floor((Math.random() * (tabCartes.length) - 1) + 1)];
+    
+        if (cartesJ1[i] !== rand) {
+            cartesJ1[i].innerHTML = `${rand}`;
+        
+    }
+
+}
