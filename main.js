8dc2b23cc3d8bddca3686cd3381e935459832f0d
Locky <inox127@hotmail.com>
2018-11-12 15:42:22 +0100

    lines console.log

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ tabCartes = [1,2,3,4,5,6,7,8,9,10];
 cartesJ1 = document.querySelectorAll(".container-j1").length;
 
 cartesJ1.innerHTML= `${tabCartes[Math.floor((Math.random() * (tabCartes.length)-1)+1)]}`;
+
+console.log("test");
