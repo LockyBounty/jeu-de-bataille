4039632ad547dac972f8ae78f9fc3def8554cb24
Locky <inox127@hotmail.com>
2018-11-13 16:37:44 +0100

    added lines

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 tabCartes1 = ["images/PNG/ace_of_diamonds.png", "images/PNG/2_of_diamonds.png", "images/PNG/3_of_diamonds.png", "images/PNG/4_of_diamonds.png", "images/PNG/5_of_diamonds.png", "images/PNG/6_of_diamonds.png", "images/PNG/7_of_diamonds.png", "images/PNG/8_of_diamonds.png", "images/PNG/9_of_diamonds.png", "images/PNG/10_of_diamonds.png"];
 tabCartes2 = ["images/PNG/ace_of_spades.png", "images/PNG/2_of_spades.png", "images/PNG/3_of_spades.png", "images/PNG/4_of_spades.png", "images/PNG/5_of_spades.png", "images/PNG/6_of_spades.png", "images/PNG/7_of_spades.png", "images/PNG/8_of_spades.png", "images/PNG/9_of_spades.png", "images/PNG/10_of_spades.png"];
 
-selectCarteJ1 = document.querySelector(".container-j1").getElementsByTagName("div");
+selectCarteJ1 = document.querySelector("#cardP1-1");
 setCarteJ1 = document.querySelector("#carte-img-j1");
+
+selectCarteJ2 = document.querySelector("#cardP2-1");
+setCarteJ2 = document.querySelector("#carte-img-j2");
 
 cartesJ1 = document.querySelector(".container-j1").getElementsByTagName("div");
 cartesJ2 = document.querySelector(".container-j2").getElementsByTagName("div");
+
+scoreA = document.querySelector("#score1-input");
+scoreB = document.querySelector("#score2-input");
 
 let rand = [];
 let rand2 = [];
@@ -20,13 +26,20 @@ for (i = 0; i < 5; i++) {
     cartesJ1[i].value = `${rand[i]}`;
     
     cartesJ2[i].innerHTML = `<img src=${rand2[i]} alt="unavailable" / >`;
-
+    cartesJ2[i].value = `${rand2[i]}`;
 
 }
 
-let attribution = () => {
+let attribution1 = () => {
     setCarteJ1.src = `${selectCarteJ1.value}`;
+    this;
+}
+let attribution2 = () => {
+    setCarteJ2.src = `${selectCarteJ2.value}`;
+    this;
 }
 
+selectCarteJ1.addEventListener('click', attribution1);
+selectCarteJ2.addEventListener('click', attribution2);
 
-selectCarteJ1.addEventListener("click", attribution )
+
